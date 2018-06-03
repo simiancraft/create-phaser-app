46691dc89f7d62dbbe4981d9c041a54034a7b792
the-simian <harlinjesse@gmail.com>
2018-06-03 12:53:34 -0500

    add the ground

--- src/scenes/start.js
+++ src/scenes/start.js
@@ -1,11 +1,18 @@
-import { HEIGHT, SCALE, WIDTH } from '../config/constants';
-
 import Phaser from 'phaser/src/phaser.js';
 import backgroundGradient from '../assets/backgrounds/start/back-gradient.png';
 import cloud1 from '../assets/backgrounds/start/cloud-1.png';
 import cloud2 from '../assets/backgrounds/start/cloud-2.png';
+import constants from '../config/constants';
+import ground from '../assets/backgrounds/start/ground.png';
 import moon from '../assets/backgrounds/start/moon.png';
 import sea from '../assets/backgrounds/start/sea.png';
+
+const { WIDTH, HEIGHT, SCALE } = constants;
+
+const center = {
+  width: WIDTH * 0.5,
+  height: HEIGHT * 0.5
+};
 
 export default class Start extends Phaser.Scene {
   constructor() {
@@ -17,9 +24,13 @@ export default class Start extends Phaser.Scene {
     this.load.image('sea', sea);
     this.load.image('cloud-1', cloud1);
     this.load.image('cloud-2', cloud2);
+    this.load.image('ground', ground);
   }
   create() {
-    this.add.image(WIDTH, HEIGHT, 'back-gradient');
+    this.add.image(center.width, center.height, 'back-gradient').setScale(2);
+    this.add.image(center.width, center.height, 'sea').setScale(2);
+
+    this.add.image(center.width, center.height, 'ground').setScale(2);
   }
   update() {}
   render() {}
