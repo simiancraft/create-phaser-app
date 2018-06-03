16babe86bb95aeb9e4d0f16e1c3d8e6a870684c1
the-simian <harlinjesse@gmail.com>
2018-06-03 12:26:30 -0500

    rename images

---
+++ src/scenes/start-menu.js
@@ -0,0 +1,14 @@
+import { HEIGHT, SCALE, WIDTH } from '../config/constants';
+
+import Phaser from 'phaser/src/phaser.js';
+import backgroundGradient from '../assets/backgrounds/start/back-gradient.png';
+
+export default class StartMenu extends Phaser.Scene {
+  constructor() {
+    super({ key: 'Game' });
+  }
+  preload() {}
+  create() {}
+  update() {}
+  render() {}
+}
