ff7e616c4668371a6914c4418e78297bb736731b
the-simian <harlinjesse@gmail.com>
2018-06-18 01:55:15 -0500

    load every animation

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -13,9 +13,6 @@ export default class Game extends Phaser.Scene {
     super({ key: 'Game' });
   }
   preload() {
-    console.log(playerJSON);
-    console.log(playerPNG);
-
     //map
     this.load.image('tilemap-rock-grass', rockTilemap);
     this.load.tilemapTiledJSON('map', level);
@@ -78,25 +75,61 @@ export default class Game extends Phaser.Scene {
   }
 
   makeAnimations() {
-    this.makeAnimation({
-      name: 'left2right-aerial',
-      frames: 5,
-      repeat: true
-    });
-    this.makeAnimation({
-      name: 'left2right-walkturn-back',
-      frames: 11,
-      repeat: true
-    });
-    this.makeAnimation({
-      name: 'left-idle',
-      frames: 46,
-      repeat: true
-    });
-    this.makeAnimation({
-      name: 'right-idle',
-      frames: 46,
-      repeat: true
+    const animations = [
+      { name: 'left-crouch-dwnup', frames: 5 },
+      { name: 'left-crouch-updwn', frames: 5 },
+      { name: 'left-crouchjump', frames: 7 },
+      { name: 'left-damage', frames: 5 },
+      { name: 'left-die', frames: 58 },
+      { name: 'left-firecannon-dwn', frames: 6 },
+      { name: 'left-firecannon-dwnfwd', frames: 6 },
+      { name: 'left-firecannon-fwd', frames: 6 },
+      { name: 'left-firecannon-up', frames: 6 },
+      { name: 'left-firecannon-upfwd', frames: 6 },
+      { name: 'left-firecannonwalk-dwn', frames: 22 },
+      { name: 'left-firecannonwalk-dwnfwd', frames: 22 },
+      { name: 'left-firecannonwalk-fwd', frames: 22 },
+      { name: 'left-firecannonwalk-up', frames: 22 },
+      { name: 'left-firecannonwalk-upfwd', frames: 22 },
+      { name: 'left-firemissile', frames: 22 },
+      { name: 'left-idle', frames: 46 },
+      { name: 'left-slide-slide2stand', frames: 3 },
+      { name: 'left-slide-stand2slide', frames: 5 },
+      { name: 'left-walk', frames: 22 },
+      { name: 'left2right-aerial', frames: 5 },
+      { name: 'left2right-walkturn-back', frames: 11 },
+      { name: 'left2right-walkturn-front', frames: 11 },
+      { name: 'right-crouch-dwn2up', frames: 5 },
+      { name: 'right-crouch-up2dwn', frames: 5 },
+      { name: 'right-crouchjump', frames: 7 },
+      { name: 'right-damage', frames: 5 },
+      { name: 'right-die', frames: 58 },
+      { name: 'right-firecannon-dwn', frames: 6 },
+      { name: 'right-firecannon-dwnfwd', frames: 6 },
+      { name: 'right-firecannon-fwd', frames: 6 },
+      { name: 'right-firecannon-up', frames: 6 },
+      { name: 'right-firecannon-upfwd', frames: 6 },
+      { name: 'right-firecannonwalk-dwn', frames: 22 },
+      { name: 'right-firecannonwalk-dwnfwd', frames: 22 },
+      { name: 'right-firecannonwalk-fwd', frames: 22 },
+      { name: 'right-firecannonwalk-up', frames: 22 },
+      { name: 'right-firecannonwalk-upfwd', frames: 22 },
+      { name: 'right-firemissile', frames: 22 },
+      { name: 'right-idle', frames: 46 },
+      { name: 'right-slide-slide2stand', frames: 3 },
+      { name: 'right-slide-stand2slide', frames: 5 },
+      { name: 'right-walk', frames: 22 },
+      { name: 'right2left-aerial', frames: 5 },
+      { name: 'right2left-walkturn-back', frames: 11 },
+      { name: 'right2left-walkturn-front', frames: 11 }
+    ];
+    animations.forEach(animation => {
+      const { name, frames, repeat } = animation;
+      this.makeAnimation({
+        name: name,
+        frames: frames,
+        repeat: !!repeat
+      });
     });
   }
 
