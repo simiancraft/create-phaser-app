6d10f8368f590a034927c3fd50b40826a1672d8c
the-simian <harlinjesse@gmail.com>
2018-06-18 02:05:20 -0500

    refactor animation list

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -2,6 +2,7 @@ import Phaser from 'phaser/src/phaser.js';
 
 import rockTilemap from '../assets/levels/rock-tilemap.png';
 import level from '../assets/levels/test-level.json';
+import playerAnimationList from '../assets/player/player-animation-list';
 import playerJSON from '../assets/player/player.json';
 import playerPNG from '../assets/player/player.png';
 import constants from '../config/constants';
@@ -36,26 +37,34 @@ export default class Game extends Phaser.Scene {
     this.mapLayerGround.setCollisionBetween(1, 36);
 
     //create player
-    this.player = this.physics.add.sprite(256, 256, 'player');
+    this.player = this.physics.add.sprite(100, 100, 'player');
     this.player.body.setGravityY(300);
     this.physics.add.collider(this.player, this.mapLayerGround);
     this.cameras.main.startFollow(this.player);
     this.makeAnimations();
+    this.player.on('animationcomplete', this.animcomplete, this);
   }
+
   update() {
     this.cursors = this.input.keyboard.createCursorKeys();
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-160);
+      this.player.anims.play('left-walk', true);
     } else if (this.cursors.right.isDown) {
       this.player.setVelocityX(160);
+      this.player.anims.play('right-walk', true);
     } else {
       this.player.setVelocityX(0);
       this.player.anims.play('left-idle', true);
     }
 
     if (this.cursors.up.isDown && this.player.body.onFloor()) {
-      this.player.setVelocityY(-200);
+      this.player.setVelocityY(-300);
     }
+  }
+
+  animcomplete(animation, frame) {
+    //console.log(animation, frame);
   }
 
   makeAnimation({ name, frames, repeat }) {
@@ -75,55 +84,7 @@ export default class Game extends Phaser.Scene {
   }
 
   makeAnimations() {
-    const animations = [
-      { name: 'left-crouch-dwnup', frames: 5 },
-      { name: 'left-crouch-updwn', frames: 5 },
-      { name: 'left-crouchjump', frames: 7 },
-      { name: 'left-damage', frames: 5 },
-      { name: 'left-die', frames: 58 },
-      { name: 'left-firecannon-dwn', frames: 6 },
-      { name: 'left-firecannon-dwnfwd', frames: 6 },
-      { name: 'left-firecannon-fwd', frames: 6 },
-      { name: 'left-firecannon-up', frames: 6 },
-      { name: 'left-firecannon-upfwd', frames: 6 },
-      { name: 'left-firecannonwalk-dwn', frames: 22 },
-      { name: 'left-firecannonwalk-dwnfwd', frames: 22 },
-      { name: 'left-firecannonwalk-fwd', frames: 22 },
-      { name: 'left-firecannonwalk-up', frames: 22 },
-      { name: 'left-firecannonwalk-upfwd', frames: 22 },
-      { name: 'left-firemissile', frames: 22 },
-      { name: 'left-idle', frames: 46 },
-      { name: 'left-slide-slide2stand', frames: 3 },
-      { name: 'left-slide-stand2slide', frames: 5 },
-      { name: 'left-walk', frames: 22 },
-      { name: 'left2right-aerial', frames: 5 },
-      { name: 'left2right-walkturn-back', frames: 11 },
-      { name: 'left2right-walkturn-front', frames: 11 },
-      { name: 'right-crouch-dwn2up', frames: 5 },
-      { name: 'right-crouch-up2dwn', frames: 5 },
-      { name: 'right-crouchjump', frames: 7 },
-      { name: 'right-damage', frames: 5 },
-      { name: 'right-die', frames: 58 },
-      { name: 'right-firecannon-dwn', frames: 6 },
-      { name: 'right-firecannon-dwnfwd', frames: 6 },
-      { name: 'right-firecannon-fwd', frames: 6 },
-      { name: 'right-firecannon-up', frames: 6 },
-      { name: 'right-firecannon-upfwd', frames: 6 },
-      { name: 'right-firecannonwalk-dwn', frames: 22 },
-      { name: 'right-firecannonwalk-dwnfwd', frames: 22 },
-      { name: 'right-firecannonwalk-fwd', frames: 22 },
-      { name: 'right-firecannonwalk-up', frames: 22 },
-      { name: 'right-firecannonwalk-upfwd', frames: 22 },
-      { name: 'right-firemissile', frames: 22 },
-      { name: 'right-idle', frames: 46 },
-      { name: 'right-slide-slide2stand', frames: 3 },
-      { name: 'right-slide-stand2slide', frames: 5 },
-      { name: 'right-walk', frames: 22 },
-      { name: 'right2left-aerial', frames: 5 },
-      { name: 'right2left-walkturn-back', frames: 11 },
-      { name: 'right2left-walkturn-front', frames: 11 }
-    ];
-    animations.forEach(animation => {
+    playerAnimationList.forEach(animation => {
       const { name, frames, repeat } = animation;
       this.makeAnimation({
         name: name,
