d414258d7c5f98c12e6e8f85a805a19d92bf539f
the-simian <harlinjesse@gmail.com>
2018-08-05 23:58:26 -0500

    replace old levels with new approach using tiled rules

---
+++ src/assets/levels/raw/level-0/input-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="input-tiles" tilewidth="16" tileheight="16" tilecount="8" columns="8">
+ <image source="input-tiles.png" width="128" height="16"/>
+</tileset>
