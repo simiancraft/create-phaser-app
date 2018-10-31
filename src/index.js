273c54ad7cbf394c3502d5549ea18cfeec66ea93
the-simian <harlinjesse@gmail.com>
2018-10-31 11:28:17 -0500

    readd the laoding screen

--- src/index.js
+++ src/index.js
@@ -21,10 +21,7 @@ const config = {
       debug: false
     }
   },
-  scene: [
-    //  StartScene,
-    GameScene
-  ],
+  scene: [StartScene, GameScene],
   pixelArt: true,
   antialias: false,
   callbacks: {
