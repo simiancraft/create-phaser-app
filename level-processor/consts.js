d73c0fc23547e9cd2108a1dd234867009a8813a7
the-simian <harlinjesse@gmail.com>
2018-11-14 01:12:56 -0600

    export consts

---
+++ level-processor/consts.js
@@ -0,0 +1,19 @@
+const BASE_WIDTH = 16;
+const BASE_HEIGHT = 16;
+const MARGIN = 0;
+const SPACING = 0;
+const COLOR = 0x00000000;
+const RAW_FOLDER = 'raw';
+const PROCESSED_FOLDER = 'processed';
+const INPUT_ONLY_PROP = 'InputOnly';
+
+export {
+  BASE_WIDTH,
+  BASE_HEIGHT,
+  MARGIN,
+  SPACING,
+  COLOR,
+  RAW_FOLDER,
+  PROCESSED_FOLDER,
+  INPUT_ONLY_PROP
+};
--- level-processor/main.js
+++ level-processor/main.js
@@ -20,18 +20,19 @@ import globby from 'globby';
 import tileExtruder from 'tile-extruder';
 
 import {
+  BASE_HEIGHT,
+  BASE_WIDTH,
+  COLOR,
+  INPUT_ONLY_PROP,
+  MARGIN,
+  PROCESSED_FOLDER,
+  RAW_FOLDER,
+  SPACING
+} from './consts';
+import {
   moveLayerImages,
   templateImageImportFile
 } from './level-image-processing';
-
-const BASE_WIDTH = 16;
-const BASE_HEIGHT = 16;
-const MARGIN = 0;
-const SPACING = 0;
-const COLOR = 0x00000000;
-const RAW_FOLDER = 'raw';
-const PROCESSED_FOLDER = 'processed';
-const INPUT_ONLY_PROP = 'InputOnly';
 
 const LEVELS_INPUT_PATH = path.resolve(
   __dirname,
