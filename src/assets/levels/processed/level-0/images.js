68c6b1a0246adf531b48d25e66780d6b440b6800
the-simian <harlinjesse@gmail.com>
2018-11-14 01:14:01 -0600

    retemplate images

--- src/assets/levels/processed/level-0/images.js
+++ src/assets/levels/processed/level-0/images.js
@@ -1,20 +1,14 @@
-
-  
-
-  import BackgroundGradient from './background-with-planets.png';
+import BackgroundGradient from './background-with-planets.png';
 import BackgroundSea from './mountain_and_sea.png';
 import BackgroundClouds from './clouds.png';
-  
 
-  import rockMossPlantsDoors from './rock-moss-plants-doors.png';
+import rockMossPlantsDoors from './rock-moss-plants-doors.png';
 import inputTiles from './input-tiles.png';
-  export default {
-    
-'BackgroundGradient':BackgroundGradient,
-'BackgroundSea':BackgroundSea,
-'BackgroundClouds':BackgroundClouds,
-    
-'rock-moss-plants-doors':rockMossPlantsDoors,
-'input-tiles':inputTiles
-  };
-  
+export default {
+  BackgroundGradient: BackgroundGradient,
+  BackgroundSea: BackgroundSea,
+  BackgroundClouds: BackgroundClouds,
+
+  'rock-moss-plants-doors': rockMossPlantsDoors,
+  'input-tiles': inputTiles
+};
